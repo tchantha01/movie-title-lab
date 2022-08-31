3d8bc68ad7608878c3e7b19c6c3ea32dcee00b23
Toky Chanthavong <tchantha01@gmail.com>
2022-08-31 16:40:29 -0600

    moive title

---
+++ index.js
@@ -0,0 +1,75 @@
+console.log('hello world')
+
+// Step 1: Grab an HTML element
+
+
+
+
+// Step 2: Write the function
+
+document.querySelector('form').addEventListener('submit', addMovie)
+const message = document.querySelector('#message')
+
+function addMovie(event) {
+    
+    event.preventDefault();
+    
+    let inputField = document.querySelector('input')
+    
+    const movie = document.createElement('li')
+    
+    const movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie)
+    movie.appendChild(movieTitle)
+
+    const deleteBtn = document.createElement('button')
+
+    deleteBtn.textContent = 'x'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    
+    
+
+    
+
+    const list = document.querySelector('ul')
+    
+    list.appendChild(movie);
+
+
+    
+    
+
+    inputField.value = ''
+
+}
+
+function deleteMovie(event) {
+
+    event.target.parentNode.remove()
+    message.textContent = 'Movie deleted!'
+
+
+}
+
+function crossOffMovie(event) {
+
+    event.target.classList.toggle('checked')
+    if(event.target.classList.contains('checked') === true){
+        message.textContent = 'Movie watched!'
+
+    }else {
+        message.textContent = 'Movie added back!'
+    }
+
+
+
+
+}
+
+
+
+
+
+// Step 3: Combines Step 1 and Step 2 using an Event Listener
